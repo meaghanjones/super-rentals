476051cd52c9c6889a6e62950dec0418f6067211
Bang Nguyen and Meaghan Jones <meaghan@>
2016-08-22 09:18:26 -0700

    add computed property to sort rentals by price

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  fullLocation: Ember.computed('city.cityName', 'city.country', function() {
+    return this.get('city.cityName') + ', ' + this.get('city.country');
+  }),
+
+  sortBy:['cost:asc'],
+  sortedRentals: Ember.computed.sort('city.rentals', 'sortBy'),
+
   actions: {
     destroyCity(city) {
       if (confirm('Are you sure you want to delete this city?')) {
